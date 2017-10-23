d78ffd2a449c91b0c1786701f143c314b2642806
Meir Blachman <meirblachman@gmail.com>
2017-10-23 22:54:32 +0300

    a prototype for the new visitor

---
+++ src/FluentAssertions.Analyzers.Tests/PocTests.cs
@@ -0,0 +1,137 @@
+﻿using Microsoft.CodeAnalysis.CSharp;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.IO;
+using System.Text;
+using System;
+
+using static Microsoft.CodeAnalysis.CSharp.SyntaxFactory;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+using System.Collections.Generic;
+
+namespace FluentAssertions.Analyzers.Tests
+{
+    [TestClass]
+    public class PocTests
+    {
+        [TestMethod]
+        public void ReplaceMultipleMethodsWithOtherMethods()
+        {
+            string Template(string assertion) => new StringBuilder()
+                .AppendLine("using System.Collections.Generic;")
+                .AppendLine("using System.Linq;")
+                .AppendLine("using System;")
+                .AppendLine("using FluentAssertions;")
+                .AppendLine("namespace PocNamespace")
+                .AppendLine("{")
+                .AppendLine("    public class Program")
+                .AppendLine("    {")
+                .AppendLine("        public static void Main()")
+                .AppendLine("        {")
+                .AppendLine("            var nestedList = new List<List<int>>();")
+                .AppendLine($"            {assertion}")
+                .AppendLine("        }")
+                .AppendLine("    }")
+                .AppendLine("}")
+                .ToString();
+
+            // old: .B() ... .D()
+            // new: .H() ...
+
+            var oldAssertion = "nestedList.Should().NotBeNull(\"because I said {0} so\".Substring(\"because\".Length), Environment.MachineName).And.ContainSingle().Which.Should().NotBeEmpty();";
+            var newAssertion = "a.A().H(\"The Reason {0} is so {1}\", \"this\", 7).C.E.F(\"go to {0} and then {1} !.\", \"hell\", true);";
+
+            var oldSource = Template(oldAssertion);
+            var newSource = Template(newAssertion);
+
+            var statement = ParseStatement(oldAssertion);
+
+            var walker = new PocCSharpSyntaxWalker("Should", "NotBeNull", "And", "NotBeEmpty");
+            statement.Accept(walker);
+
+            var result = walker.IsValid;
+
+            /*
+            DiagnosticVerifier.VerifyCSharpDiagnostic<PocAnalyzer>(oldSource);
+            DiagnosticVerifier.VerifyCSharpFix<PocCodeFix, PocAnalyzer>(oldSource, newSource);
+            */
+            var nestedList = new List<List<int>>();
+            nestedList.Should().NotBeNull("because I said {0} so".Substring("because".Length + 1), Environment.MachineName).And.ContainSingle().Which.Should().NotBeEmpty();
+        }
+
+        [TestMethod]
+        [Ignore]
+        public void MyTestMethod()
+        {
+            const string testClassName = "DictionaryTests";
+            string input = $@"d:\Visual Studio Projects\FluentAssertions.Analyzers\src\FluentAssertions.Analyzers.Tests\Tips\{testClassName}.cs";
+            string output = $@"d:\Visual Studio Projects\FluentAssertions.Analyzers\src\FluentAssertions.Analyzers.Tests\Tips\{testClassName}.generated.cs";
+
+            var compilation = ParseCompilationUnit(File.ReadAllText(input));
+
+            var rewriter = new AttributesCSharpSyntaxRewriter();
+            var newCompilation = compilation.Accept(rewriter);
+
+            var newCode = newCompilation.ToFullString()
+                .Replace("(            ", "(\n            ")
+                .Replace(",            ", ",\n            ");
+
+            File.WriteAllText(output, $"{newCode}");
+        }
+        private class AttributesCSharpSyntaxRewriter : CSharpSyntaxRewriter
+        {
+            const string prefix = "ToDictionary(p => p.Key, p=> p.Value)";
+            const string postfix = "And.ToString()";
+            static string AddToText(string text) => text
+                .Replace("actual.", $"actual.{prefix}.")
+                .Replace(";", $".{postfix};");
+
+
+            public override SyntaxNode VisitMethodDeclaration(MethodDeclarationSyntax node)
+            {
+                if (node.AttributeLists.Count == 0) return node;
+
+                var attributeLists = node.AttributeLists.RemoveAt(node.AttributeLists.Count - 1);
+
+                foreach (var list in node.AttributeLists.RemoveAt(node.AttributeLists.Count - 1))
+                {
+                    var attribute = list.Attributes[0];
+                    switch (attribute.Name)
+                    {
+                        case IdentifierNameSyntax identifier when identifier.Identifier.Text == "AssertionDiagnostic":
+                            var argument = (LiteralExpressionSyntax)attribute.ArgumentList.Arguments[0].Expression;
+
+                            var newText = AddToText(argument.Token.Text);
+
+                            attributeLists = attributeLists.Add(AttributeList().AddAttributes(
+                                attribute.WithArgumentList(AttributeArgumentList().AddArguments(
+                                    AttributeArgument(LiteralExpression(SyntaxKind.StringLiteralExpression, ParseToken(newText)))
+                                    ))
+                                ).WithTriviaFrom(list));
+                            break;
+                        case IdentifierNameSyntax identifier when identifier.Identifier.Text == "AssertionCodeFix":
+                            var oldAssertion = (LiteralExpressionSyntax)attribute.ArgumentList.Arguments[0].Expression;
+                            var newAssertion = (LiteralExpressionSyntax)attribute.ArgumentList.Arguments[1].Expression;
+
+                            var oldAssertionText = AddToText(oldAssertion.Token.Text);
+                            var newAssertionText = AddToText(newAssertion.Token.Text);
+
+                            attributeLists = attributeLists.Add(AttributeList().AddAttributes(
+                                attribute.WithArgumentList(AttributeArgumentList().AddArguments(
+                                    attribute.ArgumentList.Arguments[0].WithExpression(LiteralExpression(SyntaxKind.StringLiteralExpression, ParseToken(oldAssertionText))),
+                                    attribute.ArgumentList.Arguments[1].WithExpression(LiteralExpression(SyntaxKind.StringLiteralExpression, ParseToken(newAssertionText)))
+                                    ).WithTriviaFrom(attribute.ArgumentList))
+                                ).WithTriviaFrom(list));
+                            break;
+                        default:
+                            break;
+                    }
+                }
+
+                attributeLists = attributeLists.Add(node.AttributeLists.Last());
+
+                return node.WithAttributeLists(attributeLists);
+            }
+        }
+    }
+}
---
+++ src/FluentAssertions.Analyzers/Poc/PocAnalyzer.cs
@@ -0,0 +1,134 @@
+﻿using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.Diagnostics;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+using Microsoft.CodeAnalysis.CSharp;
+using System.Collections.Generic;
+using System.Collections.Immutable;
+using System.Linq;
+using System;
+
+namespace FluentAssertions.Analyzers
+{
+    [DiagnosticAnalyzer(LanguageNames.CSharp)]
+    public class PocAnalyzer : DiagnosticAnalyzer
+    {
+        public const string DiagnosticId = "PocAnalyzer";
+        public const string Category = "POC";
+        public const string Title = "POC analyzer";
+
+        public const string Message = "POC analyzer tree searcher";
+
+        public override ImmutableArray<DiagnosticDescriptor> SupportedDiagnostics => ImmutableArray.Create(Rule);
+
+        private readonly DiagnosticDescriptor Rule = new DiagnosticDescriptor(DiagnosticId, Title, Message, Category, DiagnosticSeverity.Info, true);
+
+        public override void Initialize(AnalysisContext context)
+        {
+            context.EnableConcurrentExecution();
+            context.ConfigureGeneratedCodeAnalysis(GeneratedCodeAnalysisFlags.None);
+            context.RegisterCodeBlockAction(AnalyzeCodeBlock);
+        }
+
+        private void AnalyzeCodeBlock(CodeBlockAnalysisContext context)
+        {
+            var method = context.CodeBlock as MethodDeclarationSyntax;
+            if (method == null) return;
+
+            if (method.Body != null)
+            {
+                foreach (var statement in method.Body.Statements.OfType<ExpressionStatementSyntax>())
+                {
+                    var diagnostic = AnalyzeExpression(statement.Expression);
+                    if (diagnostic != null)
+                    {
+                        context.ReportDiagnostic(diagnostic);
+                    }
+                }
+                return;
+            }
+            if (method.ExpressionBody != null)
+            {
+                var diagnostic = AnalyzeExpression(method.ExpressionBody.Expression);
+                if (diagnostic != null)
+                {
+                    context.ReportDiagnostic(diagnostic);
+                }
+            }
+        }
+
+        private Diagnostic AnalyzeExpression(ExpressionSyntax expression)
+        {
+            var walker = new PocCSharpSyntaxWalker("Should", "NotBeNull", "And", "NotBeEmpty");
+            expression.Accept(walker);
+
+            var result = walker.IsValid;
+
+            return null;
+        }
+
+
+    }
+
+    public class PocCSharpSyntaxWalker : CSharpSyntaxWalker
+    {
+        public readonly string[] SpecialProperties = { "And", "Which" };
+
+        public ImmutableStack<string> AllMembers { get; }
+        public ImmutableStack<string> Members { get; private set; }
+
+        public bool IsValid { get; private set; }
+
+        public PocCSharpSyntaxWalker(params string[] members)
+        {
+            AllMembers = ImmutableStack.Create(members);
+            Members = AllMembers;
+        }
+
+        public override void VisitMemberAccessExpression(MemberAccessExpressionSyntax node)
+        {
+            if (Members.IsEmpty)
+            {
+                IsValid = true;
+                return;
+            }
+
+            var name = node.Name.Identifier.Text;
+            Console.WriteLine($"MemberAccess: {name}, Members: {string.Join(",", Members)}");
+
+            if (SpecialProperties.Contains(Members.Peek()))
+            {
+                Members = Members.Pop();
+            }
+            else if (node.Parent.IsKind(SyntaxKind.InvocationExpression))
+            {
+                if (Members.Peek() == name)
+                {
+                    Members = Members.Pop();
+                }
+            }
+            else
+            {
+                Members = AllMembers;
+            }
+
+            Visit(node.Expression);
+        }
+
+        public override void VisitInvocationExpression(InvocationExpressionSyntax node)
+        {
+            Visit(node.Expression);
+        }
+
+        /*
+        private int _indent = 0;
+        public override void Visit(SyntaxNode node)
+        {
+            _indent++;
+            var indent = new string(' ', _indent * 2);
+            Console.WriteLine($"{indent}{node.GetType().Name}");
+            base.Visit(node);
+            --_indent;
+        }
+        */
+    }
+}
